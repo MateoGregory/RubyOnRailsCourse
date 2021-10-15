b3050c0b76fec20684ee8a2a6a76faeed9f0526a
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 10:11:03 -0500

    Section 4 class 43

---
+++ Section4/43.multilinestr.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+words = <<MLS
+  This will be a multi-line string
+  when is output.
+
+  The tabs will also be preserver
+  Just you watch
+
+  Goodbye!
+MLS
+
+puts(words)
