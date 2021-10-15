b6efd1791179161032a4e95b6abee9cc33b296b3
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 10:04:52 -0500

    Section 4 class 42

---
+++ Section4/42.string.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+puts("Hi teher, I'm a string")
+puts('I can also include characters and dnumbers: # $ ! @ 59')
+
+name = 'Boris'
+renevnue = '$23 dollars'
+
+puts(name, renevnue)
+
+space = ' '
+p(space.length)
+
+puts
+
+empty = ''
+p(empty.length)
+
+p(name.class)
+p(space.class)
+p(revenue.class)
+p(empty.class)
+
+str = String.new('The value of this string')
+p(str)
