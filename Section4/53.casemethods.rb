6c71a91184be634f1c6d12a2f3c9453d2b3c1a3d
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 12:06:51 -0500

    Section 4 class 53

---
+++ Section4/53.casemethods.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+puts('hello'.capitalize)
+puts('heLLo'.capitalize)
+puts('Hello'.capitalize)
+
+puts('hello world @!54'.capitalize)
+puts('hello world'.capitalize)
+
+puts('hello world @!54'.upcase)
+puts('hello world'.downcase)
+
+puts('heLLo'.swwapcase)
