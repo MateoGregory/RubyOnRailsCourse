2a53eef073f9e5386a1623ff6e0408801263b652
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 11:00:18 -0500

    Section 4 class 47

---
+++ Section4/47.strconcats.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+first_name = 'Harry'
+last_name = 'Potter'
+
+p(first_name + last_name)
+
+p(first_name.concat(last_name))
+p(first_name.prepend(last_name))
+
+p(first_name << ' ' << last_name)
