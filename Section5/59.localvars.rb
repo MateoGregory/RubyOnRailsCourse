a631d37cee2bf55acc15e6c799174d1e5ebee20d
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 13:07:50 -0500

    Section 5 class 59

--- Section5/59.localvars.rb
+++ Section5/59.localvars.rb
@@ -1,1 +1,11 @@
 # frozen_string_literal: true
+
+name = 'Mateo'
+last_name = 'Gregory'
+birth_date = 1999
+
+def introduce_myself(name, last_name, birth_date)
+  p("Hello, my name is #{name} #{last_name}, I born at #{birth_date}")
+end
+
+introduce_myself(name, last_name, birth_date)
