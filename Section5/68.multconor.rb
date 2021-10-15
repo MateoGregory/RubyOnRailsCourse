31583da5123136aab4139182061fad7f2a86f0c4
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 15:03:50 -0500

    Section 5 class 68

---
+++ Section5/68.multconor.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+budget = 5
+price = 10
+mood = 'happy'
+
+if budget > price || mood == 'happy'
+  p('I\'m going to buy this!')
+else
+  p('Nah i dont what nor can affor this item')
+end
