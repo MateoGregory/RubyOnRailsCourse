6041c81431d881e8e80ed3a22cfdcc9f134e13cc
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 14:41:58 -0500

    Section 5 class 65

---
+++ Section5/65.elseif.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+# Commented due to rubocop warnings (Expect case-when clause)
+# color = 'green'
+
+# Commented due to rubocop warnings (Expect case-when clause)
+# if color == 'red'
+#   p('red is rad')
+# elsif color == 'green'
+#   p('green is great')
+# elsif color == 'yellow'
+#   p('yellow is great')
+# end
