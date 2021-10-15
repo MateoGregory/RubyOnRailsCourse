1c670f7292b9ab646d451c164905f057e967c453
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 10:18:08 -0500

    Section 4 class 44

---
+++ Section4/44.escape.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+puts('Hello World')
+
+puts("Juliet said 'Goodbye' to Romeo")
+puts('Juliet said "Goodbye" to Romeo')
+
+puts('Juliet said \'Goodbye\' to Romeo')
+puts('Juliet said \n\'Goodbye\' to Romeo')
+puts('\tJuliet said \n\'Goodbye\' to Romeo')
