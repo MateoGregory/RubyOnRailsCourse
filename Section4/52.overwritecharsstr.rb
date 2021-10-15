f17307d6acbf50a65ae65ac463f5b17cbd816c72
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 12:00:17 -0500

    Section 4 class 52

---
+++ Section4/52.overwritecharsstr.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+thing = 'rocker ship'
+p(thing)
+
+thing[0] = 'P'
+p(thing)
+
+thing[1] = 'a'
+p(thing)
+
+thing[9] = '0'
+p(thing)
+
+fact = 'I love blueberry pie'
+p(fact)
+
+fact[7..10] = 'rasp'
+p(fact)
+
+fact[2..5] = 'absoluteley adore'
+p(fact)
