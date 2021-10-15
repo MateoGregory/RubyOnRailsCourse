bdf00130b018816df3874e651297b5b923f2fdf4
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-15 14:36:31 -0500

    Section 5 class 64

---
+++ Section5/64.truthfalse.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+p('this is true') if 5 < 8
+
+# Commented due to rubocop warnings
+# p('this is true') if 0
+# p('this is true') if 1
+# p('this is true') if -15
+# p('this is true') if ''
+# p('this is true') if 'this is a string'
+# p('this is true') if nil
