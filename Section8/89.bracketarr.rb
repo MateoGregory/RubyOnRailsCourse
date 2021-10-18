5ecf55bebe4527b6164ac190298eb31c02207b84
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-18 17:09:45 -0500

    section 8 lesson 87 arrays

---
+++ Section8/89.bracketarr.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+numbers = [1, 2, 3, 4, 5, 6]
+p(numbers)
+puts(numbers)
+print(numbers)
+
+letters = %w[sugar book car dice]
+p(letters)
+puts(letters)
+print(letters)
+
+things = [4, true, 'hello', 10.99]
+p(things)
+
+arr = [[1, 2, 3], [1, 2, 3]]
+
+p(arr)
+puts(arr)
+print(arr)
