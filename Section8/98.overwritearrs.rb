5810ca78515fe19c962d7ce684aa09b2b1278d0b
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-19 09:07:17 -0500

    Section 8 class 92 to 99

--- Section8/98.overwritearrs.rb
+++ Section8/98.overwritearrs.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-arr = 1..20
+arr = Array.new(20, 1)
 p(arr)
 
 arr[4] = 'NaN'
 
 arr[4, 7] = %w[NaN NaN]
 
-arr[8..11] = %w[Complex number]
+arr[8..11] = 'Complex number'
+
+p(arr)
