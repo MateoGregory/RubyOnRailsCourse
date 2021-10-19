ab87c6d62e203f4327596d850f5f397aeb322f9d
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-19 15:13:40 -0500

    Section 8 class 126

---
+++ Section9/126.find_index.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+colors = %w[red purple orange blue]
+p(colors.find_index('red'))
+p(colors.find_index('orange'))
+p(colors.find_index('magenta'))
+
+p(colors.index('red'))
+p(colors.index('orange'))
+p(colors.index('magenta'))
