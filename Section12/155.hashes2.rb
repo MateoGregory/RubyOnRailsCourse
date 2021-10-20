3901d3ac9409dfef8c315ff88b0db663bcefa626
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-20 15:44:33 -0500

    Section 12 class 155

---
+++ Section12/155.hashes2.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+a = {
+  'key1' => 'value1',
+  'key2' => 'value2',
+  'key3' => 'value3',
+  'key4' => 'value4',
+  'key5' => 'value5',
+}
+a['key1']
+a['key2']
+a['key3']
+a['key4']
+a['key5']
+
+a.class
