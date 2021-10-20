b2df5b345ee55807519d235459fca5910920c8cf
MateoGregory <mateo.gregory.kommit@gmail.com>
2021-10-20 15:56:42 -0500

    Section 12 class 160

---
+++ Section12/160.lengthempty.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+shopping_list = {
+  banana: 5,
+  oranges: 10,
+  carrots: 3,
+  crackers: 15
+}
+
+vegan_shopping_list = {}
+
+p(shopping_list.length)
+p(vegan_shopping_list.length)
+
+p(shopping_list.empty?)
+p(vegan_shopping_list.empty?)
